c13423951762ba71dd0845a006faffe8f8a46897
noamk15 <151642601+noamk15@users.noreply.github.com>
2026-05-17 14:24:37 +0300

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -1,11 +1,11 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
 class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        # Objective function: f(x) = x^2
-        # Derivative:         f'(x) = 2x
-        # Update rule:        x = x - learning_rate * f'(x)
-        # Round final answer to 5 decimal places
-        actual = init
-        for _ in range(iterations):
-            derivate = 2*actual
-            actual = actual - (learning_rate * derivate)
-        return round(actual, 5)
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        return np.round(1 / (1+ np.exp(-z)), 5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        return np.maximum(0, z)
